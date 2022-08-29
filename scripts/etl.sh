8d3d24f60058addac2e92515578b3fdca8328aff
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2022-08-28 23:01:41 -0600

    update etl process so it can be run inside container

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -1,3 +1,3 @@
-docker compose exec pyclient python src/ddl.py
-docker compose exec pyclient python src/seed.py
-docker compose exec pyclient python src/report.py
+python /app/src/ddl.py
+python /app/src/seed.py
+python /app/src/report.py
